78e4e82cce1d8ca72bfb28fe71ecf0b11c3b1c7d
Jack Ditto <jackditto7@gmail.com>
2019-04-02 15:43:45 -0400

    Update database_helper.py
    Read data from excel to database.

--- database_helper.py
+++ database_helper.py
@@ -1,63 +1,71 @@
 from api import db, Class
+import pandas as pd
+from pandas import ExcelFile
 
-classes = [
-    {
-        "class_name": "Computer Science",
-        "crn": "1421",
-        "professor": "Greg Rogers",
-        "start_time": "9:00am",
-        "end_time": "10:15am",
-        "location": "JPSN 103",
-        "department": "CMSC"
-    },
-    {
-        "class_name": "Calculus",
-        "crn": "5264",
-        "professor": "Paul Smith",
-        "start_time": "11:00am",
-        "end_time": "12:15pm",
-        "location": "JPSN 132",
-        "department": "MATH"
-    },
-    {
-        "class_name": "Spanish in the Media",
-        "crn": "8562",
-        "professor": "Leslie Kissling",
-        "start_time": "3:00pm",
-        "end_time": "5:00pm",
-        "location": "INTC 210",
-        "department": "LAIS"
-    },
-    {
-        "class_name": "Intro to Microeconomics",
-        "crn": "2142",
-        "professor": "Grace Vanderwergen",
-        "start_time": "2:00pm",
-        "end_time": "3:15pm",
-        "location": "BUS 312",
-        "department": "ECON"
-    },
-    {
-        "class_name": "Who Do You Trust?",
-        "crn": "6342",
-        "professor": "Carol Wittig",
-        "start_time": "8:00am",
-        "end_time": "9:15am",
-        "location": "LIB 204",
-        "department": "FYS"
-    }
-]
+# Excel file to read from
+df = pd.read_excel("201820_ABJ 2.xlsx", sheet_name="Sheet1")
 
-for c in classes:
+# Number of columns
+num_classes = len(df[df.columns[0]])
+
+# Days of the week for building week code
+days = ["MON", "TUE", "WED", "THU", "FRI", "SAT"]
+
+# JSON of class days and time
+times = {}
+
+
+def updateTimesJson(i, times_json, df):
+    for d in days:
+        if not str(df[d][i]) == "nan":
+            if not d in times_json:
+                times_json[d] = [
+                    [
+                        df["S TM"][i].strftime("%H:%M"),
+                        df["E TM"][i].strftime("%H:%M")
+                    ]
+                ]
+            else:
+                times_json[d].append(
+                    [
+                        df["S TM"][i].strftime("%H:%M"),
+                        df["E TM"][i].strftime("%H:%M")
+                    ]
+                )
+    return times_json
+
+
+# Iterate through columns
+for i in range(num_classes):
+
+
+    times = updateTimesJson(i, times, df)
+
+    while i < num_classes and str(df["CRN"][i]) == str(df["CRN"][i+1]):
+        
+
+    # Build class object from data in file
     obj = Class(
-        class_name = c["class_name"],
-        crn = c["crn"],
-        professor = c["professor"],
-        start_time = c["start_time"],
-        end_time = c["end_time"],
-        location = c["location"],
-        department = c["department"]
+        campus = df["CAMPUS"][i],
+        crn = str(df["CRN"][i]),
+        department = df["SUBJ"][i],
+        course_number = str(df["CRSE"][i]),
+        section = str(df["SEC"][i]),
+        class_name = df["TITLE"][i],
+        attr = str(df["ATTR"][i]),
+        num_credits = str(df["CR"][i]),
+        times = times,
+        building = df["BLDG"][i],
+        room = str(df["RM"][i]),
+        code = str(df["CODE"][i]),
+        professor = df["INSTRUCTOR"][i],
+        notes = df["COMMENTS"][i]
     )
-    db.session.add(obj)
+
+
+
+    classes_list.append(obj)
+
+# db.session.add(obj)
 
 db.session.commit()
